b3b0e516a3a82deb8199beebe1f2affab2d29ed9
YuYue-meg <157476402+YuYue-meg@users.noreply.github.com>
2024-06-25 16:55:28 +0100

    added home page and reroute the nav bar and removed repeated header and footer in idividual pages and added them to app.js

--- frontendreact/src/pages/FeedPage.js
+++ frontendreact/src/pages/FeedPage.js
@@ -5,16 +5,10 @@ import Footer from '../components/footer.js';
 function FeedPage() {
   return (
     <div>
-      <header id='Header'>
-        <Header />
-      </header>
       <main>
         <h1>Feed Page</h1>
         <p>This is the feed page</p>
       </main>
-      <footer>
-        <Footer />
-      </footer>
     </div>
   );
 }

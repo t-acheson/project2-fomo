23390daf0351733aefe1511bcddd0fe68a0efb29
t-acheson <taraacheson@gmail.com>
2024-06-26 16:33:57 +0100

    footer test passes when ran locally

--- frontendreact/src/tests/footer.test.js
+++ frontendreact/src/tests/footer.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; // for the additional matchers
-import Footer from './Footer'; // adjust the path if necessary
+import Footer from '../components/footer';
 
 // Start of Footer test
 test('renders footer with copyright info', () => {

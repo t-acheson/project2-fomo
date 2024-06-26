3f01064537c0e05c1e3122926ed1f5460216ee29
t-acheson <taraacheson@gmail.com>
2024-06-26 17:57:20 +0100

    added sample test for go

--- go/recommendations_test.go
+++ go/recommendations_test.go
@@ -1,16 +1,33 @@
-// package main
+package main
 
-// import (
-// 	"context"
-// 	"log"
-// 	"os"
-// 	"strings"
-// 	"testing"
+import (
+	// "context"
+	// "log"
+	// "os"
+	// "strings"
+	"testing"
 
-// 	pb "backend/grpc-client"
+	pb "backend/grpc-client"
 
-// 	"google.golang.org/grpc"
-// )
+	// "google.golang.org/grpc"
+)
+
+
+func TestPass(t *testing.T) {
+	// Test case
+	expected := 10
+	result := add(5, 5)
+
+	// Assertion
+	if result != expected {
+		t.Errorf("Expected %d, but got %d", expected, result)
+	}
+}
+
+// add is a helper function to add two numbers
+func add(a, b int) int {
+	return a + b
+}
 
 // func TestPrintRecommendation(t *testing.T) {
 // 	//connect to gRPC server

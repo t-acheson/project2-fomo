c92224a6a6a9a8d7b8d942a548a6271af0245c9f
t-acheson <taraacheson@gmail.com>
2024-06-27 17:30:10 +0100

    passes 3 render tests - page, header, paragraph text

--- frontendreact/src/tests/MapPage.test.js
+++ frontendreact/src/tests/MapPage.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import MapPage from '../pages/MapPage';
 
 describe('MapPage', () => {
-    it('renders without crashing', () => {
+    beforeEach(() => {
         render(<MapPage />);
+    });
+
+    it('renders without crashing', () => {});
+
+    it('renders the header text', () => {
+        const headerElement = screen.getByRole('heading', { name: /map Page/i });
+        expect(headerElement).toBeInTheDocument();
+    });
+
+    it('renders the paragraph text', () => {
+        const paragraphElement = screen.getByText(/This is the map page/i);
+        expect(paragraphElement).toBeInTheDocument();
     });
 });

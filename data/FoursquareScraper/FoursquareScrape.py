9d47fb3f97a92b4227ebe658997802e387aa270e
Muir0con <muireann0con@gmail.com>
2024-06-28 16:11:38 +0100

    Update FoursquareScraper
    Removed commented out initial exploratory attempts at scraper, added in if statements for remaining boroughs. Commented code for collaboration and tested further to ensure code iterates through all boroughs and all neighbourhoods in boroughs

--- data/FoursquareScraper/FoursquareScrape.py
+++ data/FoursquareScraper/FoursquareScrape.py
@@ -1,80 +1,29 @@
-# Foursquare Scraper initial testing and attempts
-
-#* Commented out code bellow was initial testing of foursquare API
-#import requests
-
-#url = "https://api.foursquare.com/v3/places/match"
-
-#params = {
-  	#"name": "Foursquare",
-    #"address": "50 W 23rd St",
-    #"city": "New York",
-    #"state": "New York",
-    #"postalCode": "10010",
-    #"cc": "US",
-    #"ll": "43.000351,-75.499901",
-#}
-
-#headers = {
-    #"Accept": "application/json",
-    #"Authorization": "fsq39ABlAtI+mIE6JF0tuXHBscUcSmPl2heIDngD/xaobE4="
-#}
-
-#response = requests.request("GET", url, params=params, headers=headers)
-
-#print(response.text)
-
-
-# Get back list of places in New York
-#import requests
-
-#url = "https://api.foursquare.com/v3/places/search?near=New%20York&limit=50"
-
-#headers = {
-    #"accept": "application/json",
-    #"Authorization": "fsq39ABlAtI+mIE6JF0tuXHBscUcSmPl2heIDngD/xaobE4="
-    #}
-
-#response = requests.get(url, headers=headers)
-
-#print(response.text)
-
-#TODO Think I need to take the list of places output here and feed it into Venues to get venue tips
-#TODO researching how to do this and venue tips
-
-#! Think Foursquare limits to 50 responses per call
-#* maybe instead of looking for all of new york could get 50 results per date? per neighbourhood and per category? 
-#import requests
-
-#url = "https://api.foursquare.com/v3/places/search?fields=tips&near=New%20York&limit=50"
-
-#headers = {
-    #"accept": "application/json",
-    #"Authorization": "fsq39ABlAtI+mIE6JF0tuXHBscUcSmPl2heIDngD/xaobE4="
-#}
-
-#response = requests.get(url, headers=headers)
-
-#print(response.text)
+# FOURSQUARE SCRAPER
 
 #* If I could make 50 calls per category per neighbourhood in NY
+#* Calculated 90 neighbourhoods X 6 categoriess in each neighbourhood X 50 responses per category
+#? 100 neighbourhoods gives exactly 30,000 responses - confident that would stay in limit
+#* ~27,000 returned, API allows 30,000 within limit
 
-# Manhattan = 45 = 14% = 6
-# Bronx = 56 = 17% = 10
-# Staten Island = 66 = 20% = 13
-# Brooklyn = 77 = 23% = 18
-# Queens = 85 = 26% = 22
-#69 total
+# 329 neighbourhoods in new york as per this list: https://www.baruch.cuny.edu/nycdata/population-geography/neighborhoods.htm
+# Manhattan = 45 = 14% of total = 13 of 90
+# Bronx = 56 = 17% = 15 
+# Staten Island = 66 = 20% = 18
+# Brooklyn = 77 = 23% = 21
+# Queens = 85 = 26% = 23
+
+# 90 neighbourhoods subsampled for out purposes
+#TODO run initially by incrementing the neighbourhoods - run 5 or 10 times changing the neighbourhoods each time
 
 # New York Boroughs
 boroughs = [
-    "manhattan", "bronx", #"statenIsland", "brooklyn", "queens"
+    "manhattan", "bronx", "statenIsland", "brooklyn", "queens"
 ]
 
-
+#! Uncomment different neighbourhoods for each new API call to ensure we're getting different and varied results
 # Manhattan neighborhoods
 manhattan = [
-    "Battery Park City", "Carnegie Hill", #"East Harlem", "Financial District", "Gramercy", "Hamilton Heights",
+    "Battery Park City", "Carnegie Hill", "East Harlem", #"Financial District", "Gramercy", "Hamilton Heights",
     #"Inwood", "Lenox Hill", "Manhattan Valley", "NoHo", "Roosevelt Island", "SoHo",
     #"Times Square", "Union Square", "Wall Street", "Yorkville", "Beekman Place",
     #"Chelsea", "East Village", "Flatiron", "Greenwich Village"
@@ -82,53 +31,54 @@ manhattan = [
 
 # Bronx neighborhoods
 bronx = [
-    "Allerton", "Bathgate", #"Castle Hill", "East Tremont", "Fieldston", "High Bridge",
+    "Allerton", "Bathgate", "Castle Hill", "East Tremont", "Fieldston", #"High Bridge",
     #"Kingsbridge", "Longwood", "Marble Hill", "North Riverdale", "Olinville", "Parkchester",
     #"Riverdale", "Schuylerville", "Throgs Neck", "Unionport", "Van Nest", "Wakefield",
     #"Baychester", "City Island"
 ]
 
 # Staten Island neighborhoods
-#statenIsland = [
-    #"Annadale", "Bay Terrace", "Castleton Corners", "Dongan Hills", "Egbertville", "Fox Hills", "Graniteville",
+statenIsland = [
+    "Annadale", "Bay Terrace", "Castleton Corners", "Dongan Hills", "Egbertville", "Fox Hills", #"Graniteville",
     #"Heartland Village", "Lighthouse Hill", "Manor Heights", "New Brighton", "Oakwood", "Park Hill", "Randall Manor",
     #"Sandy Ground", "Todt Hill", "Ward Hill", "Arden Heights", "Bloomfield", "Charleston"
-#]
+]
 
 # Brooklyn neighborhoods
-#brooklyn = [
-    #"Bath Beach", "Canarsie", "Ditmas Park", "East Flatbush", "Farragut",
-    #"Georgetown", "Highland Park", "Kensington", "Manhattan Beach", "Navy Yard",
+brooklyn = [
+    "Bath Beach", "Canarsie", "Ditmas Park", "East Flatbush", "Farragut",
+    "Georgetown", "Highland Park", "Kensington", "Manhattan Beach", #"Navy Yard",
     #"Ocean Hill", "Paerdegat Basin", "Red Hook", "Sea Gate", "Tompkins Park North", "Vinegar Hill",
     #"Weeksville", "Bay Ridge", "Carroll Gardens", "Downtown", "East New York", "Flatbush"
-#]
+]
 
 # Queens neighborhoods
-#queens = [
-    #"Arverne", "Bay Terrace", "Cambria Heights", "Douglaston", "East Elmhurst", "Far Rockaway",
-    #"Glen Oaks", "Hammels", "Jackson Heights", "Kew Gardens", "Laurelton", "Malba", "Neponsit",
+queens = [
+    "Arverne", "Bay Terrace", "Cambria Heights", "Douglaston", "East Elmhurst", "Far Rockaway",
+    "Glen Oaks", "Hammels", "Jackson Heights", "Kew Gardens", "Laurelton", #"Malba", "Neponsit",
     #"Oakland Gardens", "Pomonok", "Queens Village", "Ravenswood", "Seaside", "Utopia",
-    #"Whitestone", "Astoria"
-#]
+    #"Whitestone", "Astoria", "Bayside", "Clearview"
+]
 
-# 7 categories to stay under 30,000
+# 6 categories to stay under 30,000
 # Foursquare categories - broad
 categories = [
     #"19000"	Travel and Transportation
-    #18000	Sports and Recreation
+    "18000"	#Sports and Recreation
     #17000	Retail
-    #16000	Landmarks and Outdoors
+    "16000"	#Landmarks and Outdoors
     #15000	Health and Medicine
-    #14000	Event
-    #13065	Dining and Drinking > Restaurant
+    "14000"	#Event
+    "13065"	#Dining and Drinking > Restaurant
     "13003"	#Dining and Drinking > Bar
     #12000	Community and Government
     #11000	Business and Professional Services
     #10052	Arts and Entertainment > Strip Club
     #10043	Arts and Entertainment > Performing Arts Venue > Theater
     #10039	Arts and Entertainment > Performing Arts Venue > Music Venue
+    #10035	Arts and Entertainment > Performing Arts Venue
     #10051	Arts and Entertainment > Stadium
-    #10032	Arts and Entertainment > Night Club
+    "10032"	#Arts and Entertainment > Night Club
     #10013	Arts and Entertainment > Dance Hall
     #10010	Arts and Entertainment > Comedy Club
     #10007	Arts and Entertainment > Carnival
@@ -151,6 +101,7 @@ import configkey
 # Connect to the database server again
 # Now specifying "summer" schema
 # mysql.connector
+#TODO need to adjust to correctly read from config file and need to update to work for Postgres
 connection = configdb.MySQL_CONFIG.connect(
     host="host",
     user="user",
@@ -180,8 +131,8 @@ CREATE TABLE IF NOT EXISTS Comments (
 
 
 try:
-    #Drop table if table weather_NY already exists
-    #cursor.execute("DROP TABLE IF EXISTS weather_NY")
+    #Drop table if table Comments already exists
+    #cursor.execute("DROP TABLE IF EXISTS Comments")
     #Create new table - with above commented out, new table only created if doesn't exist
     cursor.execute(sql)
     #Save the changes made to schema
@@ -190,57 +141,113 @@ except Exception as e:
     #print out error message
     print(e)
     
-
+# start: function to commit values to Comments table previously created
 def comments_to_db(name, address, comment, created_at, latitude, longitude):
+    # Convert created_at to useable datetime format 
     if created_at is not None:
         created_at = datetime.datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d %H:%M:%S')
         
     vals = (name, address, comment, created_at, latitude, longitude)
+    # Insert into table - list name, address etc. again to specify the column heading of table to insert vals into
     cursor.execute("INSERT INTO Comments (name, address, comment, created_at, latitude, longitude) VALUES (%s, %s, %s, %s, %s, %s)", vals)
     connection.commit()
+# end : function to commit Comments to database
                         
-    
-
-#url = "https://api.foursquare.com/v3/places/search?categories=19000&fields=name%2Ctips%2Clocation%2Cpopularity&near=Allerton%2CNY&limit=10"
-
+# Define parameters for API i.e. API key
 headers = {
     "accept": "application/json",
-    "Authorization": configkey.FoursquareAPIKEY
+    "Authorization": configkey.FoursquareAPIKey
 }
 
+# Initialise empty list for data responses from each borough
 responses = []
 
+# Set a delay time in seconds to wait between each API call
+delay = 5
+
+
+#TODO update limit on URLs to max 50, was set to 1/2 for testing
+# Initialise for loop to iterate through boroughs
 for borough in boroughs:
     if borough == "manhattan":
+        # Initialise for loop to iterate through manhattan neighbourhoods
         for m in manhattan:
+            #Initialise for loop to iterate through categories
             for category in categories:
-                url = f"https://api.foursquare.com/v3/places/search?categories={category}&fields=name%2Ctips%2Clocation%2Cpopularity&near={m}%2CNY&limit=2"
+                # use string to insert each iterable of category and m into the url
+                url = f"https://api.foursquare.com/v3/places/search?categories={category}&fields=name%2Ctips%2Clocation%2Cpopularity&near={m}%2CNY&limit=1"
                 responsem = requests.get(url, headers=headers)
                 data1 = responsem.json()
+                # Add API response data (stored in variable data1) to responses list
                 responses.append(data1)
+                print("Please wait... retrieving data for Manhattan neighborhoods")
+                time.sleep(delay)  # Wait before the next API call
                 
-    if borough == "bronx":
+    # what follows is the same as above for each of the remaining boroughs and their associated neighbourhoods            
+    elif borough == "bronx":
         for br in bronx:
             for category in categories:
-                url = f"https://api.foursquare.com/v3/places/search?categories={category}&fields=name%2Ctips%2Clocation%2Cpopularity&near={br}%2CNY&limit=2"
+                url = f"https://api.foursquare.com/v3/places/search?categories={category}&fields=name%2Ctips%2Clocation%2Cpopularity&near={br}%2CNY&limit=1"
                 responsebr = requests.get(url, headers=headers)
                 data2 = responsebr.json()
                 responses.append(data2)
+                print("Please wait... retrieving data for Bronx neighborhoods")
+                time.sleep(delay)
+    
+    elif borough == "statenIsland":
+        for s in statenIsland:
+            for category in categories:
+                url = f"https://api.foursquare.com/v3/places/search?categories={category}&fields=name%2Ctips%2Clocation%2Cpopularity&near={s}%2CNY&limit=1"
+                responsebr = requests.get(url, headers=headers)
+                data3 = responsebr.json()
+                responses.append(data3)
+                print("Please wait... retrieving data for Staten Island neighborhoods")
+                time.sleep(delay)
+    
+    elif borough == "brooklyn":
+        for b in brooklyn:
+            for category in categories:
+                url = f"https://api.foursquare.com/v3/places/search?categories={category}&fields=name%2Ctips%2Clocation%2Cpopularity&near={b}%2CNY&limit=1"
+                responsebr = requests.get(url, headers=headers)
+                data4 = responsebr.json()
+                responses.append(data4)
+                print("Please wait... retrieving data for Brooklyn neighborhoods")
+                time.sleep(delay)
+    
+    elif borough == "queens":
+        for q in queens:
+            for category in categories:
+                url = f"https://api.foursquare.com/v3/places/search?categories={category}&fields=name%2Ctips%2Clocation%2Cpopularity&near={q}%2CNY&limit=1"
+                responsebr = requests.get(url, headers=headers)
+                data5 = responsebr.json()
+                responses.append(data5)
+                print("Please wait... retrieving data for Queens neighborhoods")
+                time.sleep(delay)
 
+# uncomment below to visualise code response
 print(responsem.text, responsebr.text)
 
+# Initialise for loop to iterate through the data elements in responses list
 for data in responses:
+    # Initialise for loop iterating through results in each data element
     for result in data.get('results', []):
+                # Extract specific data to be inserted into table
                 name = result.get('name')
                 address = result['location'].get('formatted_address')
                 latitude = data['context']['geo_bounds']['circle']['center']['latitude']
                 longitude = data['context']['geo_bounds']['circle']['center']['longitude']
 
+                # if tips are in data response, extract tips info and insert into Comments table
                 if 'tips' in result:
                     for tip in result['tips']:
                         comment = tip.get('text')
                         created_at = tip.get('created_at')
+                        # Call comments_to_db function to insert data into table
                         comments_to_db(name, address, comment, created_at, latitude, longitude)
                 else:
                     comments_to_db(name, address, None, None, latitude, longitude)
 
+
+# Can close the cursor and connection with below commands
+cursor.close()
+connection.close()

dd800ce100f753869213ad59da9e640e314fcad5
Xinnan Tu <lucetu109@gmail.com>
2024-06-30 23:30:43 +0100

    Create Context file and get location button

---
+++ frontendreact/src/components/getLocationButton.js
@@ -0,0 +1,7 @@
+import Button from 'react-bootstrap/Button';
+
+// A button that ask for user location to activate the map or the message board
+<Button variant="primary" href="/map">
+    Activate
+    <i className="fas fa-chevron-right"></i>
+</Button>

c8166d77e89fa4cb77ab9d10ef5da38417e3e285
Muir0con <muireann0con@gmail.com>
2024-06-30 14:28:02 +0100

    Update EventScraper
    Minor modifications made to Event scraper code to function for postgres - now just need to modify the server details in the config file to get it working on our postgres server

--- data/EventScraper/configdb.py
+++ data/EventScraper/configdb.py
@@ -3,3 +3,12 @@ MySQL_CONFIG = {
     'user': 'root',
     'password': 'Sally2018'
 }
+
+# Currently only for local Postgres
+#TODO Adjust for our Postgres server
+Postgres_CONFIG = {
+    'host': 'localhost',
+    'user': 'muireannoconnor',
+    'password': "Summer2024",
+    'dbname': 'postgres'
+}

9c17d800268ecf3963e017758a3e0463ccab73b0
Conor-O-Mahony <conor.omahony@ucdconnect.ie>
2024-07-01 19:14:19 +0100

    Retrieve historical rows on connection

--- go/chat.go
+++ go/chat.go
@@ -34,6 +34,14 @@ func (s *Server) handleWebSocket(ws *websocket.Conn) {
   fmt.Println("New incoming connection from client:", ws.RemoteAddr())
 
   s.conns[ws] = true
+
+  // Retreive historical messages
+  comments, err := s.retrieve(ws)
+  if err != nil {
+    fmt.Println("Error retrieving historical comments")
+  } else {
+
+  }
   
   //s.mu.Unlock()
   s.readLoop(ws)
@@ -41,6 +49,34 @@ func (s *Server) handleWebSocket(ws *websocket.Conn) {
 
   fmt.Println("Client disconnected at", ws.RemoteAddr())
   delete(s.conns, ws)
+}
+
+// Select applicable historical comments and iteratively send them to the new client
+func (s *Server) retrieve(ws *websocket.Conn) {
+  rows, err = db.Query(`
+    SELECT timestamp, text, ST_Y(location) as lat, ST_X(location) as lng
+    FROM comments
+    `) // WHERE ...
+  defer rows.Close()
+  
+  for rows.Next() {
+    var comment Comment
+    if err := rows.Scan(&comment.Timestamp, &comment.Text, &comment.Lat, &comment.Lng); err != nil {
+      fmt.Println("Error retrieving comment from database:", err)
+      continue
+    }
+    //comments = append(comments, comment)
+    if message, err := json.Marshall(comment); err != nil {
+      fmt.Println("Error mashalling comment:", err)
+      continue
+    }
+    if _, err := ws.Write(message); err != nil {
+      fmt.Println("Write error:", err)
+    }
+  }
+  //if err = rows.Err(); err != nil {
+  //  fmt.Println("Error rows.Err():", err)
+  //}
 }
 
 func (s *Server) readLoop(ws *websocket.Conn) {
@@ -59,8 +95,23 @@ func (s *Server) readLoop(ws *websocket.Conn) {
     if err := json.Unmarshal(msg, &comment); err != nil {
       fmt.Println("Error unmarshalling json:", err)
       continue //Likely the users fault so we want the application to continue
-    } 
+    }
 
+    // Insert the comment into the database
+    s.insertComment(comment)
+
+    //comment := Comment{Timestamp: time.Now().UnixNano(), Text: string(msg)}
+    s.broadcast(comment) // Broadcast the message to all clients
+
+    //s.mu.Lock()
+    //comments = append(comments, comment)
+    //s.mu.Unlock()
+
+    // Then, place the comment in the database
+  }
+}
+
+func (s *Server) insertComment(comment Comment) {
     comment.Timestamp = time.Now().UTC()
 
     _, err = db.Exec(`
@@ -73,15 +124,6 @@ func (s *Server) readLoop(ws *websocket.Conn) {
       continue // Same reason as continue above
     }
 
-    //comment := Comment{Timestamp: time.Now().UnixNano(), Text: string(msg)}
-    s.broadcast(comment) // Broadcast the message to all clients
-
-    //s.mu.Lock()
-    //comments = append(comments, comment)
-    //s.mu.Unlock()
-
-    // Then, place the comment in the database
-  }
 }
 
 func (s *Server) broadcast(comment Comment) {

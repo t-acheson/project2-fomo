63ac07f64568dbcfd89b1d2dc65e30c6ff617dae
t-acheson <taraacheson@gmail.com>
2024-07-03 11:48:58 +0100

    mapPage now passing

--- frontendreact/src/tests/MapPage.test.js
+++ frontendreact/src/tests/MapPage.test.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import MapPage from '../pages/MapPage';
+import { LoadScript, GoogleMap } from '@react-google-maps/api';
+
+// Mock the LoadScript and GoogleMap components
+jest.mock('@react-google-maps/api', () => ({
+  LoadScript: ({ children }) => <div>{children}</div>,
+  GoogleMap: ({ mapContainerStyle }) => (
+    <div data-testid="google-map" style={mapContainerStyle}></div>
+  )
+}));
 
 describe('MapPage', () => {
-    beforeEach(() => {
-        render(<MapPage />);
-    });
+  beforeEach(() => {
+    render(<MapPage />);
+  });
 
-    it('renders without crashing', () => {});
+  it('renders without crashing', () => {});
 
-    it('renders the header text', () => {
-        const headerElement = screen.getByRole('heading', { name: /map Page/i });
-        expect(headerElement).toBeInTheDocument();
-    });
-
-    it('renders the paragraph text', () => {
-        const paragraphElement = screen.getByText(/This is the map page/i);
-        expect(paragraphElement).toBeInTheDocument();
-    });
+  it('renders the Google Map', () => {
+    const googleMapElement = screen.getByTestId('google-map');
+    expect(googleMapElement).toBeInTheDocument();
+  });
 });
+

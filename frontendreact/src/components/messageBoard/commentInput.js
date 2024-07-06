648fb4d3ee1fb2ce779fc52528b22b1c1dd8b0ed
Xinnan Tu <lucetu109@gmail.com>
2024-07-06 03:20:01 +0100

    send message but can't check if it is saved.

--- frontendreact/src/components/messageBoard/commentInput.js
+++ frontendreact/src/components/messageBoard/commentInput.js
@@ -1,32 +1,37 @@
 import React, { useState, useContext } from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
-import { LocationContext } from '../../App';  
+import { LocationContext } from '../../App'; 
+// import sendMessage function to send the message to the server
+import { sendMessage } from '../../hooks/webSocket';  
 
-const CommentInput = ({ onSend }) => {
-    const [text, setText] = useState('');
-    const location = useContext(LocationContext);
+const CommentInput = () => {
+    const [text, setText] = useState(''); //text is the message that the user types in the input field
+    const location = useContext(LocationContext); 
     // location is an object with `lat` and `lng` properties looks like : {lat: 53.2643606, lng: -6.157174}
     const { lat, lng } = location || { lat: 40.7128, lng: -74.0060 };
     
-    const handleInputChange = (event) => {
+    //handleInputChange function to update the text state
+    const handleInputChange = (event) => { 
         setText(event.target.value);
     };
 
-    const handleSubmit = () => {
-        const timestamp = new Date().toISOString();
-        const message = { timestamp, text, lat, lng };
-        onSend(message);
+    //handleSubmit function to generate message's attributes 
+    const handleSubmit = () => { 
+        const message = {text, lat, lng };
+        console.log('Formating message:', message);
+        sendMessage(message);
         setText('');
     };
 
     return (
+        //InputGroup component for the input field 
         <InputGroup className="mb-3">
             <Form.Control
                 placeholder="Enter your message"
                 value={text}
                 onChange={handleInputChange}
             />
-            <Button variant="primary" onClick={handleSubmit}>
+            <Button variant="primary" onClick={handleSubmit}> 
                 Submit
             </Button>
         </InputGroup>
--- frontendreact/src/hooks/webSocket.js
+++ frontendreact/src/hooks/webSocket.js
@@ -20,4 +20,18 @@ socket.addEventListener('close', function (event) {
     console.log('WebSocket connection closed:', event);
 });
 
+
+// Function to send the user input message to the server
+const sendMessage = (message) => {
+    console.log('Attempting to send message:', JSON.stringify(message));  // Log before attempting to send
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify(message));
+        console.log('Message sent successfully');  // Log on successful send
+    } else {
+        console.error('WebSocket is not open. Message not sent.');
+    }
+};
+
+
 export default socket;
+export { sendMessage };

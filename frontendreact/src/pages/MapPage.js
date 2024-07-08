a93b8c0af10326db9660513f20cdf25ea57e4e08
Xinnan Tu <lucetu109@gmail.com>
2024-07-08 13:00:19 +0100

    install the Leaflet and display OpenStreetMap.

--- frontendreact/src/pages/MapPage.js
+++ frontendreact/src/pages/MapPage.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { useContext } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import { LocationContext } from '../App'; // import LocationContext
 import TestLocationButton from '../components/mapFeatures/TestLocationButton';
-import UserLocationMarker from '../components/mapFeatures/userLocationMarker';
 
-const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 const mapContainerStyle = {
   width: '100%',
   height: '80vh'
@@ -14,30 +13,24 @@ const center = {
   lat: 40.7128,
   lng: -74.0060
 };
-const mapOptions = {
-  streetViewControl: false, // disable street view
-};
 
 function MapPage() {
-  // store the user's location in the `userPosition` variable
   const userPosition = useContext(LocationContext);
+
   return (
-    <div>
+    <div style={{ height: '100vh', width: '100%' }}>
       <main>
-        <div>
-          <LoadScript googleMapsApiKey={apiKey}>
-            <GoogleMap
-              mapContainerStyle={mapContainerStyle}
-              center={userPosition || center} // center the map at the user's location if available, otherwise center it at the default location
-              // So in the development process,if we do not fake location in the console,the center will be in Dublin.
-              zoom={10}
-              options={mapOptions}
-            >
-           {/* pass the `userPosition` variable to the `UserLocationMarker` component and render a marker at the user's location */}
-              <UserLocationMarker position={userPosition} />
-            </GoogleMap>
-          </LoadScript>
-        </div>
+        <MapContainer center={center} zoom={13} style={mapContainerStyle}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {userPosition && (
+            <Marker position={[userPosition.lat, userPosition.lng]}>
+              <Popup>You are here!</Popup>
+            </Marker>
+          )}
+        </MapContainer>
       </main>
       <TestLocationButton />
     </div>

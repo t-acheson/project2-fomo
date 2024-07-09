2269d6b5ae864170d1826807b6279e98b0e60900
YuYue-meg <157476402+YuYue-meg@users.noreply.github.com>
2024-07-09 14:36:13 +0100

    changed FOMOTaxiMap.js to a js file. added taxi zone geojason layer to front end map

--- frontendreact/src/pages/MapPage.js
+++ frontendreact/src/pages/MapPage.js
@@ -3,16 +3,39 @@ import BaseMap from '../components/map/BaseMap';
 import UserMarker from '../components/map/UserMarker';
 import GeoJSONLayer from '../components/map/GeoJSONLayer';
 import TestLocationButton from '../components/map/TestLocationButton';
+import geoLocation from '../data/FOMOTaxiMap';
+import {GeoJSON} from 'react-leaflet'
+
+console.log('geoLocation:', geoLocation);
 
 function MapPage() {
+  if (!geoLocation || !geoLocation.features) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div style={{ height: '80vh', width: '100%' }}>
       <BaseMap>
         <UserMarker />
-        <GeoJSONLayer />
-        {/* more layer or controller */}
+        {geoLocation.features.map((taxizone, index) => {
+          const coordinates = taxizone.geometry.coordinates;
+          return (
+            <GeoJSON
+              key={index}
+              data={taxizone}
+              pathOptions={{
+                fillColor: '#FBD3C',
+                fillOpacity: 0.7,
+                weight: 2,
+                opacity: 1,
+                dashArray: 3,
+                color: 'white'
+              }}
+            />
+          );
+        })}
+        <TestLocationButton />
       </BaseMap>
-      <TestLocationButton />
     </div>
   );
 }

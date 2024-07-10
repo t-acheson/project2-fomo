0938b8b724ec74c4cdc00cdb4406cd4c8f67e0cc
Xinnan Tu <lucetu109@gmail.com>
2024-07-10 17:00:25 +0100

    plan the map feature structure and add example JSON.

---
+++ frontendreact/src/components/map/mapStyle.js
@@ -0,0 +1,18 @@
+export const defaultStyle = {
+    fillColor: '#FBD3C',
+    fillOpacity: 0.2,
+    weight: 1.5,
+    opacity: 1,
+    dashArray: '3',
+    color: '#4033AA'
+  };
+  
+  export const highlightStyle = {
+    dashArray: '',
+    fillColor: '#BD0026',
+    fillOpacity: 0.7,
+    weight: 2,
+    opacity: 1,
+    color: '#E6E200'
+  };
+  
--- frontendreact/src/pages/MapPage.js
+++ frontendreact/src/pages/MapPage.js
@@ -1,68 +1,27 @@
 import React from 'react';
 import BaseMap from '../components/map/BaseMap';
 import UserMarker from '../components/map/UserMarker';
-// import GeoJSONLayer from '../components/map/GeoJSONLayer';
 import TestLocationButton from '../components/map/TestLocationButton';
+import TaxiZoneGeoJSON from '../components/map/readGeoJSON';
 import geoLocation from '../data/FOMOTaxiMap';
-import {GeoJSON} from 'react-leaflet'
+// if needed, import style from stylefile here
+// import { ... } from '../components/mapStyles';
 
-console.log('geoLocation:', geoLocation);
+
 
 function MapPage() {
   if (!geoLocation || !geoLocation.features) {
     return <div>Loading...</div>;
   }
 
-  const defaultStyle = {
-    fillColor: '#FBD3C',
-    fillOpacity: 0.2,
-    weight: 1.5,
-    opacity: 1,
-    dashArray: '3',
-    color: '#4033AA'
-  };
-
-  const highlightStyle = {
-    dashArray: '',
-    fillColor: '#BD0026',
-    fillOpacity: 0.7,
-    weight: 2,
-    opacity: 1,
-    color: '#E6E200'
-  };
-
   return (
     <div style={{ height: '80vh', width: '100%' }}>
       <BaseMap>
         <UserMarker />
-        {geoLocation.features.map((taxizone, index) => (
-          <GeoJSON
-            key={index}
-            data={taxizone}
-            pathOptions={defaultStyle}
-            onEachFeature={(feature, layer) => {
-              layer.on({
-                mouseover: (e) => {
-                  const targetLayer = e.target;
-                  targetLayer.setStyle(highlightStyle);
-                },
-                mouseout: (e) => {
-                  const targetLayer = e.target;
-                  targetLayer.setStyle(defaultStyle);
-                },
-                click: (e) => {
-                  const targetLayer = e.target;
-                  if (feature && feature.properties) {
-                    const { location_id } = feature.properties;
-                    console.log('Clicked location_id:', location_id);
-                  } else {
-                    console.error('Feature or properties not defined:', feature);
-                  }
-                }
-              });
-            }}
-          />
-        ))}
+        <TaxiZoneGeoJSON features={geoLocation.features} />
+        {/* Placeholder for future components */}
+        {/* <TaxiZoneInfoBox /> */}
+        {/* <TaxiZonePopup /> */}
         <TestLocationButton />
       </BaseMap>
     </div>

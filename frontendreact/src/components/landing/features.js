a661614ed81ce6523fb6ee78f98b3a69400ecb62
YuYue-meg <157476402+YuYue-meg@users.noreply.github.com>
2024-07-15 16:02:56 +0100

    changed css of features

--- frontendreact/src/components/landing/features.js
+++ frontendreact/src/components/landing/features.js
@@ -14,20 +14,20 @@ function Features() {
             <h2>With features such as</h2>
           </div>
           <Row>
-            <Col md={4} className="holder">
+            <Col md={4} className="holder" id="leftHolder">
               <h3>Real-Time Busyness Predictions:</h3>
               <p>Get live updates on how busy different areas are, so you can plan your night out effectively.</p>
             </Col>
-            <Col md={4} className="imageHolder">
+            <Col md={4} className="imageHolder" >
               <Image src={ballImage} />
             </Col>
-            <Col md={4} className="holder">
+            <Col md={4} className="holder" id="rightHolder">
               <h3>Radius-Based Anonymous Chat</h3>
               <p>Engage with other night owls within a 2 km radius! Comment, reply, like, and dislike posts to share your experiences and discover new spots nearby.</p>
             </Col>
           </Row>
           <Row>
-            <Col md={4} className="holder">
+            <Col md={4} className="holder" id="leftHolder">
               <h3>Interactive Map</h3>
               <p>Visualize hot spots and quieter zones with our easy-to-use map interface.</p>
             </Col>
@@ -35,7 +35,7 @@ function Features() {
               <h3>Top Zone Comments</h3>
               <p>See the top comment from each zone on the map page, giving you a quick overview of what's happening across the city.</p>
             </Col>
-            <Col md={4} className="holder">
+            <Col md={4} className="holder" id="rightHolder">
               <h3>Community-Driven Insights</h3>
               <p>Benefit from real-time experiences and tips shared by fellow users to make the most of your night out.</p>
             </Col>

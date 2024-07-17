69b4a3e186d4fe27a132ad1daf36963355f6197f
Muir0con <muireann0con@gmail.com>
2024-07-17 09:17:50 +0100

    Revert "Updated automatically when merge to DEV cancelled"
    This reverts commit b67954a4fc520bcd4ccce037de6b3143d1e52095.

--- frontendreact/src/components/messageBoard/commentDisplay.js
+++ frontendreact/src/components/messageBoard/commentDisplay.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
-<<<<<<< HEAD
 import LikeDislikeButton from './likeAndDislikeButton';
-=======
-import '../cssFiles/commentDisplay.css';
->>>>>>> parent of 437eaf1 (comment display works.)
 
 const CommentDisplay = ({ comments }) => {
     return (

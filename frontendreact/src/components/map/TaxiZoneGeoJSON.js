94b820d16aab5878d26325d08eb02889fc259e29
YuYue-meg <megwang99@gmail.com>
2024-07-28 14:39:58 +0100

    commented out most console log messages excpet the web socket connection logs

--- frontendreact/src/components/map/TaxiZoneGeoJSON.js
+++ frontendreact/src/components/map/TaxiZoneGeoJSON.js
@@ -17,7 +17,8 @@ const TaxiZoneGeoJSON = ({ features, onFeatureHover }) => {
   }, [features]);
 
   const fetchTopComment = async (lat, lng) => {
-    console.log('Fetching top comment for lat:', lat, 'lng:', lng);
+    // Debugging log
+    // console.log('Fetching top comment for lat:', lat, 'lng:', lng);
     try {
       const response = await fetch('topcomment', {
         method: 'POST',
@@ -26,10 +27,12 @@ const TaxiZoneGeoJSON = ({ features, onFeatureHover }) => {
         },
         body: JSON.stringify({ lat: parseFloat(lat), lng: parseFloat(lng) }),
       });
-      console.log('Response status:', response.status);
+      // Debugging log
+      // console.log('Response status:', response.status);
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
+        // Debugging log
+        // console.log(data);
 
         if (data) {
           const { comment, sentiment } = data;
@@ -51,11 +54,13 @@ const TaxiZoneGeoJSON = ({ features, onFeatureHover }) => {
           setPopupContent(content); // Update the state
           return content; // Return the formatted content
         } else {
-          console.log('No comments found in this area');
+          // Debugging log
+          // console.log('No comments found in this area');
           return 'No comments found in this area'; // Return the no comment message
         }
       } else {
-        console.log('Failed to fetch top comment');
+        // Debugging log
+        // console.log('Failed to fetch top comment');
         return 'No comments found in this area'; // Fallback content
       }
     } catch (error) {
@@ -67,17 +72,17 @@ const TaxiZoneGeoJSON = ({ features, onFeatureHover }) => {
 
 
   const fetchSentiment = async (lat, lng) => {
-    console.log('Fetching sentiment for lat:', lat, 'lng:', lng);
+    // console.log('Fetching sentiment for lat:', lat, 'lng:', lng);
     try {
       const response = await fetch('topsentiment', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ lat: parseFloat(lat), lng: parseFloat(lng) }),
       });
-      console.log('Response status:', response.status);
+      // console.log('Response status:', response.status);
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         if (data && data.sentiment !== undefined) {
           const sentiment = data.sentiment;
           return `
@@ -87,12 +92,12 @@ const TaxiZoneGeoJSON = ({ features, onFeatureHover }) => {
           `;
 
         } else {
-          console.log('Sentiment data is missing');
+          // console.log('Sentiment data is missing');
           return 'Failed to fetch sentiment'; // Fallback content
         }
 
       } else {
-        console.log('Failed to fetch sentiment');
+        // console.log('Failed to fetch sentiment');
         return 'Failed to fetch sentiment'; // Fallback content
       }
     } catch (error) {
@@ -123,9 +128,9 @@ const TaxiZoneGeoJSON = ({ features, onFeatureHover }) => {
             }
           },
           click: async (e) => {
-            console.log(e);
+            // console.log(e);
             const { lat, lng } = e.latlng;
-            console.log('Clicked lat:', lat, 'lng:', lng);
+            // console.log('Clicked lat:', lat, 'lng:', lng);
             const content = await fetchTopComment(lat, lng); // Await fetching top comment
             const sentimentContent = await fetchSentiment(lat, lng);
             
